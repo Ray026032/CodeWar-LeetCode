af3c0f724b6a1c916a71ff6f7a1e1f9c9053c4b5
Ray026032 <104053354+Ray026032@users.noreply.github.com>
2024-09-23 17:44:19 +0800

    leetcode:14-1.cs
    leetcode:214
    You are given a string s. You can convert s to a 
    palindrome
     by adding characters in front of it.
    
    Return the shortest palindrome you can find by performing this transformation.
    
     
    
    Example 1:
    
    Input: s = "aacecaaa"
    Output: "aaacecaaa"
    Example 2:
    
    Input: s = "abcd"
    Output: "dcbabcd"
     
    
    Constraints:
    
    0 <= s.length <= 5 * 104
    s consists of lowercase English letters only.

--- leetcode:14-1.cs
+++ leetcode:14-1.cs
@@ -1,1 +1,46 @@
+using System;
+using System.Collections;
+using System.Text;
+public class Solution
+{
+    public static void Main()
+    {
+        Console.WriteLine(ShortestPalindrome("abcd"));//dcba
+        Console.WriteLine(ShortestPalindrome("xzxcvb"));//bvcxzx
+        Console.WriteLine(ShortestPalindrome("aacecaaa"));//aaacecaa
+        //xzxcvb
+    }
+
+    public static string ShortestPalindrome(string s)
+    {
+        string reverseS = string.Join("", s.Reverse());
+
+        if (CheckReverse(s))
+            return s;
+
+        StringBuilder tempReverseS = new StringBuilder();
+
+        for (int i = 0; i < reverseS.Length; i++)
+        {
+            tempReverseS.Append(reverseS[i]);
+
+            if (CheckReverse(tempReverseS + s))
+                return tempReverseS.ToString() + s;
+        }
+
+        return "";
+
+        bool CheckReverse(string s)
+        {
+            int index = s.Length - 1;
+            for (int i = 0; index > i; i++, index--)
+            {
+                if (s[i] != s[index])
+                    return false;
+            }
+            return true;
+        }
+    }
+
+}
 
